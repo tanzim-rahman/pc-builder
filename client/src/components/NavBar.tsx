00f0e4f84f948bb354fde41f7b052df5618e179f
tanzim-rahman <tanzimrahman@gmail.com>
2025-02-09 12:21:15 +0600

    fixed navbar at narrow widths

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -17,14 +17,16 @@ const NavBar = ({
         <div className='container-fluid'>
           <div className='d-flex flex-row align-items-center'>
             <div className='navbar-brand'>
-              <img src='/logo.svg' alt='Logo' width='30' height='24' />
-              <span className='mx-2'>PC Builder</span>
+              <div className='d-flex flex-column flex-sm-row align-items-center justify-content-center'>
+                <img src='/logo.svg' alt='Logo' width='30' height='24' />
+                <span className='mx-sm-2'>PC Builder</span>
+              </div>
             </div>
             <div className='d-flex flex-column flex-sm-row align-items-center justify-content-center'>
-              <div className='mx-6'>
+              <div className='mx-sm-6'>
                 <span className='text-light'>
                   Total Cost:{' '}
-                  <span className='text-body-secondary mx-2'>
+                  <span className='text-body-secondary mx-sm-2'>
                     Tk.{' '}
                     {totalPrice
                       .toString()
@@ -34,19 +36,19 @@ const NavBar = ({
               </div>
               <div className='d-flex flex-row'>
                 <button
-                  className='btn btn-outline-success mx-1'
+                  className='btn btn-outline-success mx-sm-1'
                   onClick={exportParts}
                 >
                   Export
                 </button>
                 <button
-                  className='btn btn-outline-info mx-1'
+                  className='btn btn-outline-info mx-sm-1'
                   onClick={importParts}
                 >
                   Import
                 </button>
                 <button
-                  className='btn btn-outline-danger mx-1'
+                  className='btn btn-outline-danger mx-sm-1'
                   onClick={resetParts}
                 >
                   Reset
